10d6c460e3be6713addce2162cabc2af3eb10a16
hilmi02670-wq <hilmi02670@gmail.com>
2025-11-26 10:51:30 +0700

    Add files via upload

---
+++ Modul/BangunRuang.py
@@ -0,0 +1,11 @@
+def Lkubus(s):
+    return s * s * s
+def Lbalok(s, t):
+    return s * s * t
+def Ltabung(r, t):
+    return 3.14 * r * r * t
+def Lkerucut(r, t):
+    return (1/3) * 3.14 * r * r * t
+def Lprisma(l, w, t):
+    return l * w * t
+ 
---
+++ Modul/panggil.py
@@ -0,0 +1,23 @@
+import BangunDatar,BangunRuang
+
+print("")
+print("=====Menghitung Luas dan Keliling Bangun Datar=====")
+print("Hasil Luas Persegi                       :",BangunDatar.Lpersegi(4))
+print("Hasil Luas Bangun Segitiga               :",BangunDatar.Lsegitiga(3, 6))
+print("Hasil Luas Bangun Lingkaran              :",BangunDatar.Llingkaran(7))
+print("Hasil Luas Bangun Jajar Genjang          :",BangunDatar.Ljajaran_genjang(4, 8))
+print("Hasil Luas Bangun Persegi Panjang        :",BangunDatar.Lpersegi_panjang(5, 10))
+
+print("Hasil Keliling Bangun Persegi            :",BangunDatar.Kpersegi(4))
+print("Hasil Keliling Bangun Segitiga           :",BangunDatar.Ksegitiga(3, 4, 5))
+print("Hasil Keliling Bangun Lingkaran          :",BangunDatar.Klingkaran(7))
+print("Hasil Keliling Bangun Jajar Genjang      :",BangunDatar.Kjajaran_genjang(4, 8))
+print("Hasil Keliling Bangun Persegi Panjang    :",BangunDatar.Kpersegi_panjang(5, 10))
+
+print("")
+print("=====Menghitung Luas Bangun Ruang=====")
+print("Hasil Luas Bangun Kubus      :",BangunRuang.Lkubus(4))
+print("Hasil Luas Bangun Balok      :",BangunRuang.Lbalok(4, 6))
+print("Hasil Luas Bangun Tabung     :",BangunRuang.Ltabung(7, 10))
+print("Hasil Luas Bangun Kerucut    :",BangunRuang.Lkerucut(7, 10))
+print("Hasil Luas Bangun Prisma     :",BangunRuang.Lprisma(5,8,10))
